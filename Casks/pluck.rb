28e9855ac036755ebf4cf65ac29811b6a994e21d
kshift <kshift.lv@gmail.com>
2026-08-14 08:41:15 +0800

    chore: update Pluck to 1.0.1

--- Casks/pluck.rb
+++ Casks/pluck.rb
@@ -1,13 +1,13 @@
 cask "pluck" do
-  version "1.0.0"
-  sha256 "41b0f59b6dbad07ed9dfedf1443e588c0362a1586e6f4dd8d01d12b79114f99a"
+  version "1.0.1"
+  sha256 "1c1fe5bd2505b48d07bfa980e4e44b343da5e50530a81ef6605ec14603a3ab41"
 
   url "https://github.com/RuochenLyu/pluck/releases/download/v#{version}/Pluck.zip"
   name "Pluck"
   desc "Remove image backgrounds offline, on-device"
   homepage "https://pluck.aix4u.com/"
 
-  depends_on macos: ">= :tahoe"
+  depends_on macos: :tahoe
   depends_on arch: :arm64
 
   app "Pluck.app"
--- Formula/pluck.rb
+++ Formula/pluck.rb
@@ -1,13 +1,13 @@
 class Pluck < Formula
   desc "Remove image backgrounds offline, on-device (CLI)"
   homepage "https://pluck.aix4u.com/"
-  url "https://github.com/RuochenLyu/pluck/releases/download/v1.0.0/pluck-cli-1.0.0-macos-arm64.zip"
-  sha256 "7c8663daf7d49462e5fc8d1cef6547d79911c13cbf89edfaf1078fa1e4f2fede"
-  version "1.0.0"
+  url "https://github.com/RuochenLyu/pluck/releases/download/v1.0.1/pluck-cli-1.0.1-macos-arm64.zip"
+  version "1.0.1"
+  sha256 "de7f45287db0dec567c114318fe10993436b00747c9c8c39e248e12ab6c7186a"
   license "MIT"
 
-  depends_on :macos
   depends_on arch: :arm64
+  depends_on :macos
 
   def install
     bin.install "pluck"
